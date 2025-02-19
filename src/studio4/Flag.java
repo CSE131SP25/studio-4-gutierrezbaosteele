e8cc8ea99d6182d3200e36d15238126fe7c7372c
isaacgutierrez <isaacgutierrez@localhost>
2025-02-19 17:16:41 -0600

    Cool Flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,45 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setCanvasSize(1000, 600);
+		StdDraw.clear(StdDraw.BLACK);
 		
+		Color white = new Color(255, 255, 255);
+		StdDraw.setPenColor(white);
+		StdDraw.setPenRadius(0.006);
+		StdDraw.line(0,0.4,.41,.5);
+		
+		Color triangleColor = new Color(162,207,253);
+		Color purple = new Color(115,27,183);
+		double[] verticesX = {0.35,0.5,0.65};
+		double[] verticesY = {0.35,0.7,0.35};
+		
+		StdDraw.setPenRadius(0.018);
+		StdDraw.setPenColor(purple);
+		StdDraw.line(0.598, .464, 1, .364);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		//StdDraw.setPenRadius(0.012);
+		StdDraw.line(0.595, .476, 1, .376);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		//StdDraw.setPenRadius(0.012);
+		StdDraw.line(0.59, .488, 1, .388);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		//StdDraw.setPenRadius(0.012);
+		StdDraw.line(0.59, .5, 1, .4);
+		
+		StdDraw.setPenColor(Color.ORANGE);
+		//StdDraw.setPenRadius(0.012);
+		StdDraw.line(0.58, .512, 1, .412);
+		
+		StdDraw.setPenColor(Color.RED);
+		//StdDraw.setPenRadius(0.012);
+		StdDraw.line(0.575, .524, 1, .424);
+		
+		StdDraw.setPenColor(triangleColor);
+		StdDraw.setPenRadius(0.022);
+		StdDraw.polygon(verticesX, verticesY);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -20,5 +22,63 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String firstValue = in.next();
+		
+		//System.out.println(firstValue);
+		
+		int redComp = in.nextInt();
+		
+		//System.out.println(redComp);
+		
+		int greenComp = in.nextInt();
+		
+		//System.out.println(greenComp);
+		
+		int blueComp = in.nextInt();
+		
+		//System.out.println(blueComp);
+		
+		boolean isFilled = in.nextBoolean();
+		
+		//System.out.println(isFilled);
+		
+		//rectangle
+		if (firstValue.equals("rectangle")) {
+		
+		double x = in.nextDouble();
+		
+		double y = in.nextDouble();
+		
+		double halfWidth = in.nextDouble();
+		
+		double halfHeight = in.nextDouble();
+		
+		Color ourColor = new Color(redComp, greenComp, blueComp);
+		StdDraw.setPenColor(ourColor);
+		
+		if (isFilled) {
+			StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+		} else {
+			StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		}
+		
+		} else if (firstValue.equals("ellipse")) {
+			double x = in.nextDouble();
+			
+			double y = in.nextDouble();
+			
+			double halfWidth = in.nextDouble();
+			
+			double halfHeight = in.nextDouble();
+			
+			Color ourColor = new Color(redComp, greenComp, blueComp);
+			StdDraw.setPenColor(ourColor);
+			
+			if (isFilled) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			} else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
 	}
 }
